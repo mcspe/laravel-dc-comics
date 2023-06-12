5fc448afb68b2cb93b73d307c5d3e37b31bf9ed6
mcspe <mcspezzaferro@gmail.com>
2023-06-12 18:55:03 +0200

    [first commit] set repo

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,18 @@
+<header>
+  <div class="container-fluid">
+    <ul class="nav">
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">Active</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled">Disabled</a>
+      </li>
+    </ul>
+  </div>
+</header>
